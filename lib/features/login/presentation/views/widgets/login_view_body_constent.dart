d6529cc9c2e7b59490cf7f151f707106916ff229
MinaYasser100 <minayasser43@gmail.com>
2024-01-29 21:32:34 +0200

    active the validation in login view

--- lib/features/login/presentation/views/widgets/login_view_body_constent.dart
+++ lib/features/login/presentation/views/widgets/login_view_body_constent.dart
@@ -21,67 +21,72 @@ class LoginViewBodyContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Form(
-      child: Column(
-        children: [
-          const NavigationBackButton(),
-          const SizedBox(height: 70),
-          const Text(
-            'Welcome Back',
-            style: Styles.textStyle30,
-          ).animate().then().shake(),
-          const SizedBox(height: 60),
-          Text(
-            'Enter your eamil & password',
-            style: Styles.textStyle16.copyWith(
-              color: kTextGreyColor,
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          CustomTextFromField(
-            textEditingController: emailController,
-            labelText: 'Email',
-            validator: emailValidator,
-            keyboardType: TextInputType.emailAddress,
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          CustomTextFromField(
-            textEditingController: passwordController,
-            labelText: "Password",
-            validator: (value) {
-              if (value!.isEmpty) {
-                return 'Please enter your password';
-              }
-              return null;
-            },
-            obscureText: true,
-            iconData: Icons.visibility_off_outlined,
-            keyboardType: TextInputType.visiblePassword,
-            suffixOnPressed: () {},
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          CustomTextButton(
-            textButton: 'LOGIN',
-            onPressed: () {},
-          ),
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              'Forgotten Password',
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.black,
-                decoration: TextDecoration.underline,
+      key: fromkey,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const NavigationBackButton(),
+            const SizedBox(height: 70),
+            const Text(
+              'Welcome Back',
+              style: Styles.textStyle30,
+            ).animate().then().shake(),
+            const SizedBox(height: 60),
+            Text(
+              'Enter your eamil & password',
+              style: Styles.textStyle16.copyWith(
+                color: kTextGreyColor,
               ),
             ),
-          ),
-          const CreateNewAccountSection(),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            CustomTextFromField(
+              textEditingController: emailController,
+              labelText: 'Email',
+              validator: emailValidator,
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomTextFromField(
+              textEditingController: passwordController,
+              labelText: "Password",
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter your password';
+                }
+                return null;
+              },
+              obscureText: true,
+              iconData: Icons.visibility_off_outlined,
+              keyboardType: TextInputType.visiblePassword,
+              suffixOnPressed: () {},
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            CustomTextButton(
+              textButton: 'LOGIN',
+              onPressed: () {
+                if (fromkey.currentState!.validate()) {}
+              },
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Forgotten Password',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+            const CreateNewAccountSection(),
+          ],
+        ),
       ),
     );
   }
