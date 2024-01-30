94cfc2155ad66b1e1e2bc1f75b46285921299737
MinaYasser100 <minayasser43@gmail.com>
2024-01-30 17:48:37 +0200

    add firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:faithful_servant/core/helper/constant.dart';
 import 'package:faithful_servant/core/helper/get_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
