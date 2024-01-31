2dde26f3173f2db238835fe3887e91ec6de700cd
MinaYasser100 <minayasser43@gmail.com>
2024-01-31 22:14:57 +0200

    reposte pattern for register

---
+++ lib/features/register/data/register_repo/register_repo_implemente.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:faithful_servant/core/helper/constant.dart';
+import 'package:faithful_servant/features/register/data/register_repo/register_reop.dart';
+import 'package:faithful_servant/features/register/data/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class RegisterRepoImplemente implements RegisterRepo {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  @override
+  Future<void> putUserInformationInFirebase(
+      {required UserModel userModel,
+      required String userId,
+      required String church}) async {
+    if (church == selectChurch[saintGeorge]) {
+      await firestore
+          .collection(saintGeorge)
+          .doc(selectChurch[saintGeorge])
+          .collection('users')
+          .doc(userId)
+          .set(userModel.toJson());
+    }
+    if (church == selectChurch[virginMary]) {
+      await firestore
+          .collection(virginMary)
+          .doc(selectChurch[virginMary])
+          .collection('users')
+          .doc(userId)
+          .set(userModel.toJson());
+    }
+    if (church == selectChurch[saintMark]) {
+      await firestore
+          .collection(saintMark)
+          .doc(selectChurch[saintMark])
+          .collection('users')
+          .doc(userId)
+          .set(userModel.toJson());
+    }
+    if (church == selectChurch[saintMain]) {
+      await firestore
+          .collection(saintMain)
+          .doc(selectChurch[saintMain])
+          .collection('users')
+          .doc(userId)
+          .set(userModel.toJson());
+    }
+    //await firestore.collection('users').doc(userId).set(userModel.toJson());
+  }
+
+  @override
+  Future<UserCredential> registerUser(
+      {required String email, required String password}) async {
+    UserCredential userCredential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+    return userCredential;
+  }
+}
--- lib/features/register/presentation/manager/cubit/register_cubit.dart
+++ lib/features/register/presentation/manager/cubit/register_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:faithful_servant/features/register/data/register_repo/register_reop.dart';
 import 'package:faithful_servant/features/register/data/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -6,14 +7,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 part 'register_state.dart';
 
 class RegisterCubit extends Cubit<RegisterState> {
-  RegisterCubit() : super(RegisterInitial());
+  RegisterCubit(this.registerRepo) : super(RegisterInitial());
   IconData suffixIcon = Icons.visibility_off_outlined;
   bool obscurePassword = true;
   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
   IconData confirmSuffixIcon = Icons.visibility_off_outlined;
   bool obscureConfirmPassword = true;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
-
+  final RegisterRepo registerRepo;
   void changeShowPassword() {
     obscurePassword = !obscurePassword;
     suffixIcon = obscurePassword == true
@@ -39,12 +40,15 @@ class RegisterCubit extends Cubit<RegisterState> {
     required String userID,
     required String name,
     required String email,
-    required String phone,
+    required String phoneNum1,
+    required String phoneNum2,
     required String image,
     required String nationalId,
     required String privilage,
     required String church,
-    required String address,
+    required String numberOfnumber,
+    required String streetName,
+    required String addressOfArea,
     required String qualification,
     required String currentService,
   }) async {
@@ -54,16 +58,21 @@ class RegisterCubit extends Cubit<RegisterState> {
         userID: userID,
         name: name,
         email: email,
-        phone: phone,
+        phoneNum1: phoneNum1,
+        phoneNum2: phoneNum2,
         image: image,
         nationalId: nationalId,
         privilage: privilage,
         church: church,
-        address: address,
+        numberOfnumber: numberOfnumber,
+        streetName: streetName,
+        addressOfArea: addressOfArea,
         qualification: qualification,
         currentService: currentService,
       );
-      firestore.collection('users').doc(userID).set(userModel.toJson());
+      registerRepo.putUserInformationInFirebase(
+          userModel: userModel, userId: userID, church: church);
+      // firestore.collection('users').doc(userID).set(userModel.toJson());
       emit(RegisterCubitPutUserInformationInFirebaseSuccess());
     } catch (e) {
       emit(RegisterCubitPutUserInformationInFirebaseFailure(
@@ -75,29 +84,37 @@ class RegisterCubit extends Cubit<RegisterState> {
     required String name,
     required String email,
     required String password,
-    required String phone,
+    required String phoneNum1,
+    required String phoneNum2,
     required String image,
     required String nationalId,
     required String privilage,
     required String church,
-    required String address,
+    required String numberOfnumber,
     required String qualification,
+    required String streetName,
+    required String addressOfArea,
     required String currentService,
   }) async {
     emit(RegisterCubitUserRgistrationLoading());
     try {
-      UserCredential userCredential = await FirebaseAuth.instance
-          .createUserWithEmailAndPassword(email: email, password: password);
+      UserCredential userCredential =
+          await registerRepo.registerUser(email: email, password: password);
+      // await FirebaseAuth.instance
+      //     .createUserWithEmailAndPassword(email: email, password: password);
       putUserInformationInFirebase(
         userID: userCredential.user!.uid,
         name: name,
         email: email,
-        phone: phone,
+        phoneNum1: phoneNum1,
+        phoneNum2: phoneNum2,
         image: image,
         nationalId: nationalId,
         privilage: privilage,
         church: church,
-        address: address,
+        numberOfnumber: numberOfnumber,
+        streetName: streetName,
+        addressOfArea: addressOfArea,
         qualification: qualification,
         currentService: currentService,
       );
