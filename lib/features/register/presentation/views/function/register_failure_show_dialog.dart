bf0a5d2a321ef5ff83649b6d0f8aa64c7df3ae3d
MinaYasser100 <minayasser43@gmail.com>
2024-02-01 20:12:16 +0200

    state action in register view

---
+++ lib/features/register/presentation/views/function/register_failure_show_dialog.dart
@@ -0,0 +1,38 @@
+import 'package:faithful_servant/core/helper/constant.dart';
+import 'package:faithful_servant/core/helper/get_pages.dart';
+import 'package:faithful_servant/core/helper/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+Future<dynamic> registerFailureShowDialog(BuildContext context) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        backgroundColor: kDarkSecondColor,
+        title: Text(
+          'Failure Registration',
+          style: Styles.textStyle20.copyWith(color: kPrimaryColor),
+        ),
+        content: Text(
+          ' حدث خطاء اثناء التسجيل يرجي التاكد ان هدا الايميل الحقيقي الخاص بك ثم اعد المحاوله',
+          style: Styles.textStyle16.copyWith(
+            color: Colors.red,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Get.offNamed(GetPages.kWelcomeView);
+            },
+            child: const Text(
+              'Try Again',
+              style: TextStyle(color: kPrimaryColor),
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
