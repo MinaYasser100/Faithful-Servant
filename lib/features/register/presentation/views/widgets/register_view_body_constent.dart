b908ddba37f74186981d0983cf70180048b352e8
MinaYasser100 <minayasser43@gmail.com>
2024-02-20 17:54:08 +0200

    do the date text field and refactor code in register view

--- lib/features/register/presentation/views/widgets/register_view_body_constent.dart
+++ lib/features/register/presentation/views/widgets/register_view_body_constent.dart
@@ -1,15 +1,4 @@
 import 'package:faithful_servant/core/function/screen_action/custom_sanck_bar.dart';
-import 'package:faithful_servant/core/function/validator/educational_qualification_validator.dart';
-import 'package:faithful_servant/core/function/validator/address_of_area_validator.dart';
-import 'package:faithful_servant/core/function/validator/confirm_password_validator.dart';
-import 'package:faithful_servant/core/function/validator/email_validator.dart';
-import 'package:faithful_servant/core/function/validator/father_of_conession_validator.dart';
-import 'package:faithful_servant/core/function/validator/name_validator.dart';
-import 'package:faithful_servant/core/function/validator/national_id_validator.dart';
-import 'package:faithful_servant/core/function/validator/number_of_home_validator.dart';
-import 'package:faithful_servant/core/function/validator/password_validator.dart';
-import 'package:faithful_servant/core/function/validator/phone_validator.dart';
-import 'package:faithful_servant/core/function/validator/street_name_validator.dart';
 import 'package:faithful_servant/core/helper/constant.dart';
 import 'package:faithful_servant/core/helper/styles.dart';
 import 'package:faithful_servant/core/widgets/custom_text_button.dart';
@@ -19,12 +8,15 @@ import 'package:faithful_servant/features/register/presentation/manager/cubit/re
 import 'package:faithful_servant/features/register/presentation/views/function/determind_church.dart';
 import 'package:faithful_servant/features/register/presentation/views/function/register_failure_show_dialog.dart';
 import 'package:faithful_servant/features/register/presentation/views/widgets/choose_form_items.dart';
+import 'package:faithful_servant/features/register/presentation/views/widgets/image_register_widget.dart';
+import 'package:faithful_servant/features/register/presentation/views/widgets/section_one_in_register.dart';
+import 'package:faithful_servant/features/register/presentation/views/widgets/section_two_in_register.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import '../../../../../core/function/screen_action/successfully_show_dialog.dart';
-import '../../../../../core/widgets/profile_imge_widget.dart';
 
 class ReisterViewBodyContent extends StatefulWidget {
   const ReisterViewBodyContent({
@@ -42,6 +34,7 @@ class ReisterViewBodyContent extends StatefulWidget {
     required this.streetNameController,
     required this.addressOfAreaController,
     required this.phoneNum2Controller,
+    required this.dateController,
   });
 
   final GlobalKey<FormState> fromKey;
@@ -57,6 +50,7 @@ class ReisterViewBodyContent extends StatefulWidget {
   final TextEditingController streetNameController;
   final TextEditingController addressOfAreaController;
   final TextEditingController fatherOfConfessionController;
+  final TextEditingController dateController;
 
   @override
   State<ReisterViewBodyContent> createState() => _ReisterViewBodyContentState();
@@ -75,6 +69,7 @@ class _ReisterViewBodyContentState extends State<ReisterViewBodyContent> {
   final List<String> genderItems = ['ذكر', 'انثي'];
   String selectGenderItem = 'ذكر';
   String selectCurrentService = 'حضانة كيجي 1';
+  DateTime? selectedDate;
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -99,18 +94,7 @@ class _ReisterViewBodyContentState extends State<ReisterViewBodyContent> {
                   'The registration is successful, now go to the login'.tr,
               buttonText: 'Go To Login'.tr,
             );
-            widget.nameController.clear();
-            widget.emailController.clear();
-            widget.phoneNum1Controller.clear();
-            widget.phoneNum2Controller.clear();
-            widget.nationalIDController.clear();
-            widget.passwordController.clear();
-            widget.confirmPasswordController.clear();
-            widget.qualificationController.clear();
-            widget.numberOfHomeController.clear();
-            widget.streetNameController.clear();
-            widget.addressOfAreaController.clear();
-            widget.fatherOfConfessionController.clear();
+            clearRegisterTextEditingController();
             BlocProvider.of<RegisterCubit>(context).imageSelected == null;
           }
           if (state is RegisterCubitUserRgistrationFailure ||
@@ -133,17 +117,7 @@ class _ReisterViewBodyContentState extends State<ReisterViewBodyContent> {
                 ),
               ),
               const SizedBox(height: 30),
-              Center(
-                child: ProfileImageWidget(
-                  backgroundImage: NetworkImage(BlocProvider.of<RegisterCubit>(
-                              context)
-                          .imageSelected ??
-                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzb62jTDtJjG9KgljxtM0vPyWOq_16WOkIgA&usqp=CAU'),
-                  onPressed: () {
-                    BlocProvider.of<RegisterCubit>(context).getProfileImage();
-                  },
-                ),
-              ),
+              const ImgeRegisterWidget(),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: Form(
@@ -154,39 +128,19 @@ class _ReisterViewBodyContentState extends State<ReisterViewBodyContent> {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       const SizedBox(height: 20),
-                      CustomTextFromField(
-                        textEditingController: widget.nameController,
-                        labelText: 'Name'.tr,
-                        keyboardType: TextInputType.name,
-                        validator: nameValidator,
-                      ),
+                      SectionOneInRegister(widget: widget),
                       const SizedBox(height: 15),
                       CustomTextFromField(
-                        textEditingController: widget.emailController,
-                        labelText: 'email'.tr,
-                        keyboardType: TextInputType.emailAddress,
-                        validator: emailValidator,
-                      ),
-                      const SizedBox(height: 15),
-                      CustomTextFromField(
-                        textEditingController: widget.phoneNum1Controller,
-                        labelText: 'Phone number 1'.tr,
-                        validator: phoneValidator,
-                        keyboardType: TextInputType.phone,
-                      ),
-                      const SizedBox(height: 15),
-                      CustomTextFromField(
-                        textEditingController: widget.phoneNum2Controller,
-                        labelText: 'Phone number 2'.tr,
-                        validator: phoneValidator,
-                        keyboardType: TextInputType.phone,
-                      ),
-                      const SizedBox(height: 15),
-                      CustomTextFromField(
-                        textEditingController: widget.nationalIDController,
-                        labelText: 'National ID'.tr,
-                        keyboardType: TextInputType.number,
-                        validator: nationalIdValidator,
+                        textEditingController: widget.dateController,
+                        labelText: 'MM/dd/yyyy'.tr,
+                        validator: (p0) {
+                          return null;
+                        },
+                        iconData: Icons.date_range_outlined,
+                        suffixOnPressed: () {
+                          selectDate(context);
+                        },
+                        keyboardType: TextInputType.datetime,
                       ),
                       const SizedBox(height: 15),
                       ChooseFromItems(
@@ -229,104 +183,13 @@ class _ReisterViewBodyContentState extends State<ReisterViewBodyContent> {
                         },
                       ),
                       const SizedBox(height: 15),
-                      CustomTextFromField(
-                        textEditingController: widget.passwordController,
-                        labelText: 'password'.tr,
-                        iconData:
-                            BlocProvider.of<RegisterCubit>(context).suffixIcon,
-                        keyboardType: TextInputType.visiblePassword,
-                        obscureText: BlocProvider.of<RegisterCubit>(context)
-                            .obscurePassword,
-                        suffixOnPressed: () {
-                          BlocProvider.of<RegisterCubit>(context)
-                              .changeShowPassword();
-                        },
-                        validator: passwordVaildator,
-                      ),
-                      const SizedBox(height: 15),
-                      CustomTextFromField(
-                        textEditingController: widget.confirmPasswordController,
-                        labelText: 'Confirm Password'.tr,
-                        iconData: BlocProvider.of<RegisterCubit>(context)
-                            .confirmSuffixIcon,
-                        suffixOnPressed: () {
-                          BlocProvider.of<RegisterCubit>(context)
-                              .changeShowConfirmPassword();
-                        },
-                        keyboardType: TextInputType.visiblePassword,
-                        obscureText: BlocProvider.of<RegisterCubit>(context)
-                            .obscureConfirmPassword,
-                        validator: (value) => confirmPasswordValidator(
-                            value, widget.passwordController.text),
-                      ),
-                      const SizedBox(height: 15),
-                      CustomTextFromField(
-                        textEditingController: widget.numberOfHomeController,
-                        labelText: 'Number of home'.tr,
-                        validator: numberOfHomeValidator,
-                        keyboardType: TextInputType.text,
-                      ),
-                      const SizedBox(height: 15),
-                      CustomTextFromField(
-                        textEditingController: widget.streetNameController,
-                        labelText: 'Street name'.tr,
-                        validator: streetNameValidator,
-                        keyboardType: TextInputType.text,
-                      ),
-                      const SizedBox(height: 15),
-                      CustomTextFromField(
-                        textEditingController: widget.addressOfAreaController,
-                        labelText: 'Address of area'.tr,
-                        validator: addressOfAreaValidator,
-                        keyboardType: TextInputType.text,
-                      ),
-                      const SizedBox(height: 15),
-                      CustomTextFromField(
-                        textEditingController: widget.qualificationController,
-                        labelText: 'Educational qualification'.tr,
-                        validator: educationalQualificationValidator,
-                        keyboardType: TextInputType.text,
-                      ),
-                      const SizedBox(height: 15),
-                      CustomTextFromField(
-                        textEditingController:
-                            widget.fatherOfConfessionController,
-                        labelText: 'Father of confession'.tr,
-                        validator: fatherOfConessionValidator,
-                        keyboardType: TextInputType.text,
-                      ),
+                      SectionTwoInRegister(widget: widget),
                       const SizedBox(height: 50),
                       CustomTextButton(
                         textButton: 'Create Account'.tr,
                         onPressed: () {
                           if (widget.fromKey.currentState!.validate()) {
-                            BlocProvider.of<RegisterCubit>(context)
-                                .userRegistration(
-                              name: widget.nameController.text,
-                              email: widget.emailController.text,
-                              password: widget.passwordController.text,
-                              phoneNum1: widget.phoneNum1Controller.text,
-                              phoneNum2: widget.phoneNum2Controller.text,
-                              image: BlocProvider.of<RegisterCubit>(context)
-                                      .imageSelected ??
-                                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzb62jTDtJjG9KgljxtM0vPyWOq_16WOkIgA&usqp=CAU',
-                              nationalId: widget.nationalIDController.text,
-                              privilage: selectedItem,
-                              church: determindChurch(
-                                churchSelectedItem: churchSelectedItem,
-                              ),
-                              gender: selectGenderItem,
-                              numberOfnumber:
-                                  widget.numberOfHomeController.text,
-                              qualification:
-                                  widget.qualificationController.text,
-                              streetName: widget.streetNameController.text,
-                              addressOfArea:
-                                  widget.addressOfAreaController.text,
-                              currentService: selectCurrentService,
-                              fatherOfConfession:
-                                  widget.fatherOfConfessionController.text,
-                            );
+                            implementCreateNewAccountButtton(context);
                           } else {
                             BlocProvider.of<RegisterCubit>(context)
                                 .changeAutovalidateMode();
@@ -343,5 +206,62 @@ class _ReisterViewBodyContentState extends State<ReisterViewBodyContent> {
         },
       ),
     );
+  }
+
+  void implementCreateNewAccountButtton(BuildContext context) {
+    BlocProvider.of<RegisterCubit>(context).userRegistration(
+      name: widget.nameController.text,
+      email: widget.emailController.text,
+      password: widget.passwordController.text,
+      phoneNum1: widget.phoneNum1Controller.text,
+      phoneNum2: widget.phoneNum2Controller.text,
+      image: BlocProvider.of<RegisterCubit>(context).imageSelected ??
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzb62jTDtJjG9KgljxtM0vPyWOq_16WOkIgA&usqp=CAU',
+      nationalId: widget.nationalIDController.text,
+      privilage: selectedItem,
+      church: determindChurch(
+        churchSelectedItem: churchSelectedItem,
+      ),
+      gender: selectGenderItem,
+      numberOfnumber: widget.numberOfHomeController.text,
+      qualification: widget.qualificationController.text,
+      streetName: widget.streetNameController.text,
+      addressOfArea: widget.addressOfAreaController.text,
+      currentService: selectCurrentService,
+      fatherOfConfession: widget.fatherOfConfessionController.text,
+    );
+  }
+
+  void clearRegisterTextEditingController() {
+    widget.nameController.clear();
+    widget.emailController.clear();
+    widget.phoneNum1Controller.clear();
+    widget.phoneNum2Controller.clear();
+    widget.nationalIDController.clear();
+    widget.passwordController.clear();
+    widget.confirmPasswordController.clear();
+    widget.qualificationController.clear();
+    widget.numberOfHomeController.clear();
+    widget.streetNameController.clear();
+    widget.addressOfAreaController.clear();
+    widget.fatherOfConfessionController.clear();
+    widget.dateController.clear();
+  }
+
+  Future<void> selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate ?? DateTime.now(),
+      firstDate: DateTime(1950),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+        widget.dateController.text = selectedDate != null
+            ? DateFormat('MM/dd/yyyy').format(selectedDate!)
+            : '';
+      });
+    }
   }
 }
---
+++ lib/features/register/presentation/views/widgets/section_one_in_register.dart
@@ -0,0 +1,51 @@
+import 'package:faithful_servant/core/function/validator/email_validator.dart';
+import 'package:faithful_servant/core/function/validator/name_validator.dart';
+import 'package:faithful_servant/core/function/validator/phone_validator.dart';
+import 'package:faithful_servant/core/widgets/custom_text_from_field.dart';
+import 'package:faithful_servant/features/register/presentation/views/widgets/register_view_body_constent.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SectionOneInRegister extends StatelessWidget {
+  const SectionOneInRegister({
+    super.key,
+    required this.widget,
+  });
+
+  final ReisterViewBodyContent widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomTextFromField(
+          textEditingController: widget.nameController,
+          labelText: 'Name'.tr,
+          keyboardType: TextInputType.name,
+          validator: nameValidator,
+        ),
+        const SizedBox(height: 15),
+        CustomTextFromField(
+          textEditingController: widget.emailController,
+          labelText: 'email'.tr,
+          keyboardType: TextInputType.emailAddress,
+          validator: emailValidator,
+        ),
+        const SizedBox(height: 15),
+        CustomTextFromField(
+          textEditingController: widget.phoneNum1Controller,
+          labelText: 'Phone number 1'.tr,
+          validator: phoneValidator,
+          keyboardType: TextInputType.phone,
+        ),
+        const SizedBox(height: 15),
+        CustomTextFromField(
+          textEditingController: widget.phoneNum2Controller,
+          labelText: 'Phone number 2'.tr,
+          validator: phoneValidator,
+          keyboardType: TextInputType.phone,
+        ),
+      ],
+    );
+  }
+}
