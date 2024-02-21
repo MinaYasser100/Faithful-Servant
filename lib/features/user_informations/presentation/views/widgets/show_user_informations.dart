6c5d9f850ca4de4f06647a6d2aac76743455c537
MinaYasser100 <minayasser43@gmail.com>
2024-02-21 21:08:38 +0200

    do the age in user informations

--- lib/features/user_informations/presentation/views/widgets/show_user_informations.dart
+++ lib/features/user_informations/presentation/views/widgets/show_user_informations.dart
@@ -1,7 +1,9 @@
 import 'package:faithful_servant/core/helper/constant.dart';
 import 'package:faithful_servant/features/register/data/model/user_model.dart';
+import 'package:faithful_servant/features/user_informations/presentation/manager/cubit/user_informations_cubit.dart';
 import 'package:faithful_servant/features/user_informations/presentation/views/widgets/information_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 
 class ShowUserInformations extends StatelessWidget {
@@ -39,6 +41,12 @@ class ShowUserInformations extends StatelessWidget {
         InformationItem(
           itemName: 'birth date : '.tr,
           itemValue: userModel.brithDate,
+        ),
+        InformationItem(
+          itemName: 'Age : '.tr,
+          itemValue: (BlocProvider.of<UserInformationsCubit>(context)
+                  .calculateAge(userModel.brithDate))
+              .toString(),
         ),
         InformationItem(
           itemName: 'National ID : '.tr,
