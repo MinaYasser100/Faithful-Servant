accb69f473bd0cd779c637e8eefddfc16540a21b
MinaYasser100 <minayasser43@gmail.com>
2024-09-17 04:05:48 +0300

    to make show list work

--- lib/features/ma5domeen/presentation/view/widgets/ma5domeen_body.dart
+++ lib/features/ma5domeen/presentation/view/widgets/ma5domeen_body.dart
@@ -1,5 +1,8 @@
 import 'package:faithful_servant/core/helper/constant.dart';
+import 'package:faithful_servant/features/ma5domeen/presentation/manager/ma5domeen_cubit.dart';
+import 'package:faithful_servant/features/ma5domeen/presentation/manager/ma5domeen_states.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Ma5domeenBody extends StatelessWidget {
   const Ma5domeenBody({super.key, required this.nameStage});
@@ -7,19 +10,23 @@ class Ma5domeenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            itemCount: ma5domeenData.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(ma5domeenData[index].name),
-              );
-            },
-          ),
-        ),
-      ],
+    return BlocBuilder<Ma5domeenCubit, Ma5domeenStates>(
+      builder: (context, state) {
+        return Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: ma5domeenData.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(ma5domeenData[index].name),
+                  );
+                },
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
