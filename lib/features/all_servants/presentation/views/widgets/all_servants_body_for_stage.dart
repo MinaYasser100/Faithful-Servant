657677dc2a7e367db500c6d15c3c9810eb5a96f7
MinaYasser100 <minayasser43@gmail.com>
2024-09-25 05:03:04 +0300

    do the get all servnats for stage from firebase

--- lib/features/all_servants/presentation/views/widgets/all_servants_body_for_stage.dart
+++ lib/features/all_servants/presentation/views/widgets/all_servants_body_for_stage.dart
@@ -1,12 +1,47 @@
+import 'package:faithful_servant/features/all_servants/presentation/manager/all_servants_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AllServantsBodyForStage extends StatelessWidget {
-  const AllServantsBodyForStage({super.key});
-
+  const AllServantsBodyForStage({super.key, required this.currentService});
+  final String currentService;
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [],
-    );
+    return BlocBuilder<AllServantsCubit, AllServantsStates>(
+        builder: (context, state) {
+      return RefreshIndicator(
+        onRefresh: () async {
+          context
+              .read<AllServantsCubit>()
+              .getAllServantForStage(currentService: currentService);
+        },
+        child: BlocBuilder<AllServantsCubit, AllServantsStates>(
+          builder: (context, state) {
+            if (state is AllServantsCubitGetAllServantForStageFailure) {
+              return Center(
+                child: Text(
+                  state.errorMessage,
+                  textAlign: TextAlign.center,
+                ),
+              );
+            } else if (state is AllServantsCubitGetAllServantForStageSuccess) {
+              return ListView.builder(
+                itemCount: state.serviceUsers.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                      title: Text(
+                    state.serviceUsers[index].name,
+                  ));
+                },
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      );
+    });
   }
 }
--- lib/features/all_servants/presentation/views/widgets/all_servants_for_state.dart
+++ lib/features/all_servants/presentation/views/widgets/all_servants_for_state.dart
@@ -1,8 +1,11 @@
 import 'package:faithful_servant/core/helper/constant.dart';
 import 'package:faithful_servant/core/helper/styles.dart';
 import 'package:faithful_servant/core/widgets/design_body.dart';
+import 'package:faithful_servant/features/all_servants/data/repo/all_servants_repo_implement.dart';
+import 'package:faithful_servant/features/all_servants/presentation/manager/all_servants_cubit.dart';
 import 'package:faithful_servant/features/all_servants/presentation/views/widgets/all_servants_body_for_stage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 
 class AllServantsForStage extends StatelessWidget {
@@ -11,18 +14,24 @@ class AllServantsForStage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final currentService = Get.arguments as String;
-    return Scaffold(
-      backgroundColor: kPrimaryColor,
-      appBar: AppBar(
+    return BlocProvider(
+      create: (context) => AllServantsCubit(AllServantsRepoImplement())
+        ..getAllServantForStage(currentService: currentService),
+      child: Scaffold(
         backgroundColor: kPrimaryColor,
-        foregroundColor: kSecondColor,
-        title: Text(
-          currentService,
-          style: Styles.textStyle25SecondColor,
+        appBar: AppBar(
+          backgroundColor: kPrimaryColor,
+          foregroundColor: kSecondColor,
+          title: Text(
+            currentService,
+            style: Styles.textStyle25SecondColor,
+          ),
         ),
-      ),
-      body: const DesignBody(
-        widget: AllServantsBodyForStage(),
+        body: DesignBody(
+          widget: AllServantsBodyForStage(
+            currentService: currentService,
+          ),
+        ),
       ),
     );
   }
